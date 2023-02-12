0c31b8f6041d0d05700de0915909926b56e1f5aa
Taimoor <taimoor.m89@gmail.com>
2023-02-12 18:46:02 -0500

    removed native sanity from build

--- next-ecomm/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'h6hcjj71',
-    dataset: 'production'
-  }
-})
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
-const Home: NextPage = () => {
+const Home: NextPage = (): JSX.Element => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
