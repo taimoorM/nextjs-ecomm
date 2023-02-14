7dc860e851af40dac6d0ad0eec1c4f79360bfe91
Taimoor <taimoor.m89@gmail.com>
2023-02-14 18:25:35 -0500

    added more styling and links to navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 import {
   MagnifyingGlassIcon,
   ShoppingBagIcon,
-  User,
+  UserIcon,
 } from "@heroicons/react/24/solid";
 
 function Header() {
+  const session = false;
   return (
     <header className="stick top-0 z-30 flex w-full items-center justify-between bg-[#e7ecee] p-4">
       <div className="flex items-center justify-center md:w-1/5">
@@ -27,8 +28,31 @@ function Header() {
         <a className="headerLink">Support</a>
         <a className="headerLink">Business</a>
       </div>
-      <div className="">
-        <MagnifyingGlassIcon className="headerLink" />
+      <div className="flex items-center justify-center gap-x-4 md:w-1/5">
+        <MagnifyingGlassIcon className="headerIcon" />
+        <Link href="">
+          <div className="relative cursor-pointer">
+            <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500">
+              5
+            </span>
+            <ShoppingBagIcon className="headerIcon" />
+          </div>
+        </Link>
+        {session ? (
+          <Image
+            src={
+              // session.user?.image ||
+              "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
+            }
+            alt=""
+            className="cursor-pointer rounded-full"
+            width={34}
+            height={34}
+            // onClick={() => signOut()}
+          />
+        ) : (
+          <UserIcon className="headerIcon" />
+        )}
       </div>
     </header>
   );
