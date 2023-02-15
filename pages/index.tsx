ed6ba2fa5ba0769158186989678a3d9c753f5621
Taimoor <taimoor.m89@gmail.com>
2023-02-15 18:58:54 -0500

    added hero component and some styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const Home: NextPage = (): JSX.Element => {
   return (
@@ -11,6 +12,9 @@ const Home: NextPage = (): JSX.Element => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <main>
+        <Hero />
+      </main>
     </div>
   );
 };
