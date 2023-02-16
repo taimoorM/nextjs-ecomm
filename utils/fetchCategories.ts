dfabdfcdf1c941405e95f28a69bac5f197a0bd81
Taimoor <taimoor.m89@gmail.com>
2023-02-15 22:20:43 -0500

    added category typings and sanity client config

--- utils/fetchCategories.ts
+++ utils/fetchCategories.ts
@@ -3,8 +3,8 @@ export const fetchCategories = async () => {
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getCategories`
   );
 
-  //   const data = await res.json();
-  //   const categories: Category[] = data.categories;
+  const data = await res.json();
+  const categories: Category[] = data.categories;
 
-  //   return categories;
+  return categories;
 };
