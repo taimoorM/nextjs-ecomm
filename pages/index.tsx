3f21ad2209fbb124130b40dcdc3d2f9d9f502df4
Taimoor <taimoor.m89@gmail.com>
2023-02-15 20:22:41 -0500

    added tabs ui structure to promo section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import { Tab } from "@headlessui/react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
@@ -21,6 +21,31 @@ const Home: NextPage = (): JSX.Element => {
             <h2 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">
               New Promos
             </h2>
+            <Tab.Group>
+              <Tab.List className="flex justify-center">
+                {/* {categories.map((category) => (
+                  <Tab
+                    key={category._id}
+                    id={category._id}
+                    className={({ selected }) =>
+                      `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                        selected
+                          ? "borderGradient bg-[#35383C] text-white"
+                          : "border-b-2 border-[#35383C] text-[#747474]"
+                      }`
+                    }
+                  >
+                    {category.title}
+                  </Tab>
+                ))} */}
+              </Tab.List>
+              {/* <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+                <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+                <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+                <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+                <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel>
+              </Tab.Panels> */}
+            </Tab.Group>
           </div>
         </section>
       </main>
