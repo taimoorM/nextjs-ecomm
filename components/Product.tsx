279d73049fff3b50d18f68b296ff9570e956d520
Taimoor <taimoor.m89@gmail.com>
2023-02-24 23:16:35 -0500

    added product image, title, price & add to cart btn with some styling

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { urlFor } from "../sanity";
+import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 
 interface Props {
   product: Product;
@@ -7,8 +8,25 @@ interface Props {
 
 function Product({ product }: Props) {
   return (
-    <div className="relative h-64 w-full md:h-72">
-      <Image src={urlFor(product.image[0]).url()} fill alt={product.title} />
+    <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10">
+      <div className="relative h-64 w-full md:h-72">
+        <Image
+          src={urlFor(product.image[0]).url()}
+          fill
+          style={{ objectFit: "contain" }}
+          alt={product.title}
+        />
+      </div>
+      <div className="flex flex-1 items-center justify-between space-x-3">
+        <div className="space-y-2 text-xl text-white md:text-2xl">
+          <p>{product.title}</p>
+          <p>{product.price}</p>
+        </div>
+
+        <div className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 md:h-[70px] md:w-[70px]">
+          <ShoppingCartIcon className="h-8 w-8 text-white" />
+        </div>
+      </div>
     </div>
   );
 }
